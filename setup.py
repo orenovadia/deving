a1d92d4cbfd3dbe9e960b1891a98e4726aa99afe
orenovadia <oren@twiggle.com>
2018-04-12 16:46:19 +0300

    setup py and moved things

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='deving',
+    packages=['deving'],
+    version='0.1.0',
+    description='Some dev tools',
+    author='Oren',
+    author_email='orenovad@gmail.com',
+    url='https://github.com/orenovadia/deving',
+    install_requires=[
+        'click',
+        'tqdm',
+    ]
+)

4258aed54547c1130c9e2d46e93ad5b7eaef80ab
orenovadia <oren@twiggle.com>
2018-04-15 14:07:23 +0300

    Find exceptions script

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup
 
 setup(
     name='deving',
-    packages=['deving'],
+    packages=[
+        'deving',
+        'deving.lib',
+    ],
     version='0.1.0',
     description='Some dev tools',
     author='Oren',
@@ -12,5 +15,10 @@ setup(
         'click',
         'tqdm',
         'enum34',
-    ]
+    ],
+    entry_points={
+        'console_scripts': [
+            'deving_tracebacks = deving.main:find_exceptions',
+        ],
+    }
 )
